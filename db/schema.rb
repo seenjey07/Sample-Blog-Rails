ad423c5a2c65bdac1e9e634c29ba71bf085be9bf
seenjey07 <chrosales07@gmail.com>
2023-11-22 03:42:17 +0800

    Saving to include modifications made to the views/posts folder

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_21_153714) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_21_185212) do
+  create_table "albums", force: :cascade do |t|
+    t.string "caption", null: false
+    t.text "description", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
